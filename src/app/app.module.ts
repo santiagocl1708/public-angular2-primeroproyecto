124435765e657d02fe76d4e7ebccd7eb2b8d52b6
santiagocl1708 <santiagocl1708@gmail.com>
2017-09-10 08:11:27 -0500

    agregamos modulos cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {RouterModule, Routes} from '@angular/router'
+
+import {AddComponent as orderAddComponent} from './orders/add/add.component'
+import{CoustomersModule}from './coustomers/coustomers.module';
+import{OrdersModule}from './orders/orders.module';
+
 import { AppComponent } from './app.component';
+import { IndexComponent } from './customers/index/index.component';
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from './current-option/current-option.component';
+
+const appRoutes: Routes =[
+  {path: 'customers/index', component: IndexComponent},
+  {path: 'orders/add',component: orderAddComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    MenuComponent,
+    CurrentOptionComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoustomersModule,
+    OrdersModule,
+    RouterModule.forRoot(
+      appRoutes)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
